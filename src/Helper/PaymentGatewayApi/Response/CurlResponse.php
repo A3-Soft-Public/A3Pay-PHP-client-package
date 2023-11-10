0aa8f904ebf971e13247f4ea577d9a0fe2fcf5fc
tomas.bilik@a3soft.sk <tomas.bilik@a3soft.sk>
2023-11-10 01:03:37 +0100

    little bugfix

--- src/Helper/PaymentGatewayApi/Response/CurlResponse.php
+++ src/Helper/PaymentGatewayApi/Response/CurlResponse.php
@@ -6,8 +6,11 @@ use JsonException;
 
 class CurlResponse
 {
+    /** @var string|null  */
     private ?string $body = null;
+    /** @var int|null  */
     private ?int $statusCode = null;
+    /** @var string|null  */
     private ?string $errorMessage;
 
     public function __construct()
@@ -16,6 +19,7 @@ class CurlResponse
 
 
     /**
+     * Convert response body to json, on error throw @link JsonExcepiton
      * @throws JsonException
      * @return array
      */
@@ -24,38 +28,68 @@ class CurlResponse
        return json_decode($this->getBody(), true, 512, JSON_THROW_ON_ERROR);
     }
 
+    /**
+     * Return response body
+     * @return string
+     */
     public function getBody(): string
     {
-        return $this->body;
+        return $this->body ?? '';
     }
 
+    /**
+     * Return status code from response
+     * @return int
+     */
     public function getStatusCode(): int
     {
-        return $this->statusCode;
+        return $this->statusCode ?? 0;
     }
 
+    /**
+     * Return error message if any. Else return null
+     * @return string|null
+     */
     public function getErrorMessage(): ?string
     {
         return $this->errorMessage;
     }
 
-
+    /**
+     * Return if response has error
+     * @return bool
+     */
     public function hasErrorMessage(): bool {
         return $this->errorMessage !== null;
     }
 
+    /**
+     * Set response body
+     * @param string|null $body
+     * @return $this
+     */
     public function setBody(?string $body): CurlResponse
     {
         $this->body = $body;
         return $this;
     }
 
+    /**
+     * Set response status code
+     * @param int|null $statusCode
+     * @return $this
+     */
     public function setStatusCode(?int $statusCode): CurlResponse
     {
         $this->statusCode = $statusCode;
         return $this;
     }
 
+    /**
+     * Set response error message
+     * @param string|null $errorMessage
+     * @return $this
+     */
     public function setErrorMessage(?string $errorMessage): CurlResponse
     {
         $this->errorMessage = $errorMessage;
